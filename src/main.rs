ec12944d5ab70130251bdba3b90d7fdb1ac831dd
Ben Faerber <benfaerber@protonmail.com>
2025-04-06 16:33:40 -0600

    Run formatter

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use std::env;
+use lazy_static::lazy_static;
 use std::collections::HashMap as Map;
+use std::env;
 use std::fs;
 use std::io;
 use std::process::exit;
 use walkdir::WalkDir;
-use lazy_static::lazy_static;
 
 const DEFAULT_MAX_DEPTH: usize = 4;
 const SEARCH_EXTENSIONS_ENV: &'static str = "FIND_EXT_SEARCH_EXTENSIONS";
@@ -13,33 +13,29 @@ const CACHE_FILE_ENV: &'static str = "FIND_EXT_CACHE_FILE";
 const USE_CACHE_ENV: &'static str = "FIND_EXT_USE_CACHE";
 
 fn env(key: &str) -> String {
-    env::var(key)
-        .expect(&format!("Find Ext: set the enviroment variable '{key}'"))
+    env::var(key).expect(&format!("Find Ext: set the enviroment variable '{key}'"))
 }
 
 fn env_as_vec(key: &str) -> Vec<String> {
-    env(key) 
-        .split(',')
-        .map(str::to_string) 
-        .collect()
+    env(key).split(',').map(str::to_string).collect()
 }
 
 lazy_static! {
     static ref LOOK_FOR: Vec<String> = env_as_vec(SEARCH_EXTENSIONS_ENV);
     static ref DISALLOWED_FOLDERS: Vec<String> = env_as_vec(DISALLOWED_FOLDER_ENV);
     static ref CACHE_FILE: String = env(CACHE_FILE_ENV);
-    static ref USE_CACHE: bool = env(USE_CACHE_ENV).parse().unwrap_or(false); 
+    static ref USE_CACHE: bool = env(USE_CACHE_ENV).parse().unwrap_or(false);
 }
 
 #[derive(Debug, Default)]
 struct Cache {
-    folders: Map<String, String>
+    folders: Map<String, String>,
 }
 
 impl Cache {
     fn load() -> io::Result<Self> {
         let raw = fs::read_to_string(CACHE_FILE.to_string())?;
-        
+
         let mut folders: Map<String, String> = Map::new();
         let lines = raw.trim().split("\n");
         for line in lines {
@@ -56,14 +52,14 @@ impl Cache {
     }
 
     fn save(&self) {
-        let raw_data = &self.folders 
+        let raw_data = &self
+            .folders
             .iter()
-            .map(|(path, extension)| format!("{path};{extension}")) 
+            .map(|(path, extension)| format!("{path};{extension}"))
             .collect::<Vec<String>>()
             .join("\n");
-        
-        fs::write(&*CACHE_FILE, raw_data)
-            .expect("Failed to save Cache!");
+
+        fs::write(&*CACHE_FILE, raw_data).expect("Failed to save Cache!");
     }
 
     fn clear() {
@@ -76,7 +72,12 @@ impl Cache {
     }
 }
 
-fn find_extension(path: &str, depth: usize, look_for: &Vec<String>, cache_opt: &mut Option<Cache>) -> Option<String> {
+fn find_extension(
+    path: &str,
+    depth: usize,
+    look_for: &Vec<String>,
+    cache_opt: &mut Option<Cache>,
+) -> Option<String> {
     if let Some(cache) = cache_opt {
         if let Some(ext) = &cache.folders.get(path) {
             return Some((*ext).clone());
@@ -88,21 +89,19 @@ fn find_extension(path: &str, depth: usize, look_for: &Vec<String>, cache_opt: &
         .into_iter()
         .filter_map(|p| {
             if let Err(_) = p {
-                return None
+                return None;
             }
 
             let path = p.unwrap().path().to_string_lossy().to_string();
-            
-            if (*DISALLOWED_FOLDERS).iter().any(|disallowed| path.contains(disallowed)) {
-                return None
-            } 
-            
-            Some(
-                path.split(".")
-                    .last()
-                    .unwrap_or("")
-                    .into()
-            )
+
+            if (*DISALLOWED_FOLDERS)
+                .iter()
+                .any(|disallowed| path.contains(disallowed))
+            {
+                return None;
+            }
+
+            Some(path.split(".").last().unwrap_or("").into())
         })
         .collect();
 
@@ -114,41 +113,41 @@ fn find_extension(path: &str, depth: usize, look_for: &Vec<String>, cache_opt: &
         let last = counts.get(&ext).unwrap_or(&0);
         counts.insert(ext, *last + 1);
     }
-    
-    let max_ext = counts.into_iter()
+
+    let max_ext = counts
+        .into_iter()
         .max_by(|(_, v1), (_, v2)| v1.cmp(v2))
         .map(|(ext, _)| ext);
-    
+
     if let Some(ext) = max_ext {
         if let Some(cache) = cache_opt {
             cache.add(&path, &ext).save();
         }
-        Some(ext) 
+        Some(ext)
     } else {
         None
-    } 
+    }
 }
 
 fn display_help_message() {
     let msg = r#"
 Usage: find_ext PATH
     --clear (-c) = Clear cache 
-    "#.trim();
+    "#
+    .trim();
     println!("{msg}");
 }
 
-
 fn main() {
-    let mut cache = (*USE_CACHE)
-        .then_some(Cache::load_or_new());
+    let mut cache = (*USE_CACHE).then_some(Cache::load_or_new());
 
     let args: Vec<String> = env::args().collect();
     let path = match args.get(1) {
         Some(p) if p == "-c" || p == "--clear" => {
             Cache::clear();
-            println!("Cleared cache!"); 
+            println!("Cleared cache!");
             return;
-        }, 
+        }
         Some(p) => p,
         None => {
             display_help_message();
@@ -156,11 +155,12 @@ fn main() {
         }
     };
 
-    let depth = args.get(2)
+    let depth = args
+        .get(2)
         .and_then(|t| t.parse::<usize>().ok())
         .unwrap_or(DEFAULT_MAX_DEPTH);
-   
-    let attempt = find_extension(path, depth, &*LOOK_FOR, &mut cache); 
+
+    let attempt = find_extension(path, depth, &*LOOK_FOR, &mut cache);
     let output = if let None = attempt {
         find_extension(path, depth, &*LOOK_FOR, &mut cache)
     } else {
